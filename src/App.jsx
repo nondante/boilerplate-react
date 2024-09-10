50450e17b45bb6855267f82521ec65a9da6de335
Yana <nondante@gmail.com>
2024-09-10 11:35:41 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>React</h1>
+      <h1>React + Vite</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
