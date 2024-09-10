f1f289892f007c02f4693094c10f2a4eea2bcbed
nondante <you@example.com>
2024-09-10 11:06:56 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+function stringToBoolean(str) {
+  return str.toLowerCase() === "true";
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      port: env.PORT, // specify the port number you want
+    },
+    envPrefix: ["VITE_", "CLIENT_"],
+    build: {
+      // dont minify for debug builds
+      minify: stringToBoolean(env.DEBUG) ? false : "esbuild",
+      // produce sourcemaps for debug builds
+      sourcemap: stringToBoolean(env.DEBUG),
+    },
+  };
+});
